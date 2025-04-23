a6bb381699dbf6479343292b9429a9b38a2a10e9
Yoskex <merezhnikovmaksimm@yandex.ru>
2025-04-23 16:17:52 +0300

    authorization

---
+++ forms.py
@@ -0,0 +1,18 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, EqualTo
+
+class LoginForm(FlaskForm):
+    username = StringField('Имя пользователя', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Имя пользователя', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    confirm_password = PasswordField(
+        'Подтвердите пароль',
+        validators=[DataRequired(), EqualTo('password', message='Пароли должны совпадать')]
+    )
+    submit = SubmitField('Зарегистрироваться')
