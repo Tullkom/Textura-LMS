9324e9d70ac5de2f342383134638a5fffc6b4695
Tullkom <Tullkom@yandex.ru>
2025-05-09 22:00:38 +0600

    Registration, books

--- forms.py
+++ forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import TextAreaField
 from wtforms.validators import DataRequired, EqualTo
+
 
 class LoginForm(FlaskForm):
     username = StringField('Имя пользователя', validators=[DataRequired()])
@@ -16,3 +18,9 @@ class RegistrationForm(FlaskForm):
         validators=[DataRequired(), EqualTo('password', message='Пароли должны совпадать')]
     )
     submit = SubmitField('Зарегистрироваться')
+
+class BookForm(FlaskForm):
+    title = StringField('Название книги', validators=[DataRequired()])
+    content = TextAreaField("Текст произведения")
+    is_private = BooleanField("Недоступно гостям")
+    submit = SubmitField('Применить')
---
+++ main.py
@@ -0,0 +1,114 @@
+from flask import Flask, render_template, redirect, url_for, flash
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
+from forms import LoginForm, RegistrationForm, BookForm
+from data import db_session
+from data.users import User
+from data.books import Book
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+def main():
+    db_session.global_init("db/users.db")
+    session = db_session.create_session()
+    app.run()
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
+
+
+@app.route('/')
+def home():
+    return render_template('main.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        if form.password.data != form.confirm_password.data:
+            return render_template('register.html', form=form, title='Регистрация', error='diff_pass')
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.username == form.username.data).first():
+            return render_template('register.html', form=form, title='Регистрация', error='existing_user')
+        new_user = User(username=form.username.data)
+        new_user.set_password(form.password.data)
+        db_sess.add(new_user)
+        db_sess.commit()
+        flash('Вы успешно зарегистрировались!', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form, title='Регистрация')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.username == form.username.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            flash('Вы успешно вошли!', 'success')
+            return redirect(url_for('success'))
+        flash('Неверное имя пользователя или пароль.', 'danger')
+        return render_template('login.html', form=form, title='Авторизация', error='invalid_user')
+    return render_template('login.html', form=form, title='Авторизация')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
+
+@app.route('/success', methods=['GET', 'POST'])
+def success():
+    return render_template('success.html')
+
+@app.route('/popular', methods=['GET', 'POST'])
+def popular():
+    return render_template('popular.html')
+
+@app.route('/newest', methods=['GET', 'POST'])
+def newest():
+    db_sess = db_session.create_session()
+    books = db_sess.query(Book).filter(Book.is_private != True).order_by(Book.id)[::-1]
+    return render_template('newest.html', books=books)
+
+@app.route('/faq', methods=['GET', 'POST'])
+def faq():
+    return render_template('faq.html')
+
+@app.route('/about', methods=['GET', 'POST'])
+def about():
+    return render_template('about.html')
+
+@app.route('/account', methods=['GET', 'POST'])
+@login_required
+def account():
+    return render_template('account.html')
+
+@app.route('/book',  methods=['GET', 'POST'])
+@login_required
+def add_book():
+    form = BookForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        book = Book()
+        book.title = form.title.data
+        book.path = form.content.data
+        book.is_private = form.is_private.data
+        current_user.books.append(book)
+        db_sess.merge(current_user)
+        db_sess.commit()
+        return redirect('/')
+    return render_template('add_book.html', title='Добавление книги',
+                           form=form)
+
+
+if __name__ == '__main__':
+    main()
